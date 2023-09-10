1eadbe0d98661f56b97c4944d11d0a2d42e8c97e
Alberto Corona <muqui@hotmail.com>
2023-09-09 18:57:24 -0600

    .

---
+++ funciones.js
@@ -0,0 +1,17 @@
+
+ function printPage(){
+  // body =  document.body.innerHTML;
+//   elemento = document.getElementById('nombre').innerHTML
+
+// document.body.innerHTML = elemento;
+ 
+ 
+//const node = document.createElement("li");
+//const textnode = document.createTextNode("Water");
+//node.appendChild(textnode);
+//document.body.appendChild(node);
+
+ window.print();
+ //document.body.innerHTML = body;
+
+}
